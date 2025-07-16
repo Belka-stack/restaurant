be505a82368f6c82d1c1aeb0a13280d4c96526fb
Belka-stack <Belkabakouche@gmail.com>
2025-07-16 14:58:20 +0200

    Création de l'entité Booking et mise en place de la relation avec l'entité Restaurant et User plus migration en base de données

---
+++ src/Entity/Booking.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookingRepository::class)]
+class Booking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 36)]
+    private ?string $uuid = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $guestNumber = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $orderDate = null;
+
+    #[ORM\Column]
+    private ?\DateTime $orderHour = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $allergy = null;
+
+    #[ORM\Column]
+    private ?\DateTime $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'bookings')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $client = null;
+
+    #[ORM\ManyToOne(inversedBy: 'bookings')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Restaurant $restaurant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): static
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getGuestNumber(): ?int
+    {
+        return $this->guestNumber;
+    }
+
+    public function setGuestNumber(int $guestNumber): static
+    {
+        $this->guestNumber = $guestNumber;
+
+        return $this;
+    }
+
+    public function getOrderDate(): ?\DateTime
+    {
+        return $this->orderDate;
+    }
+
+    public function setOrderDate(\DateTime $orderDate): static
+    {
+        $this->orderDate = $orderDate;
+
+        return $this;
+    }
+
+    public function getOrderHour(): ?\DateTime
+    {
+        return $this->orderHour;
+    }
+
+    public function setOrderHour(\DateTime $orderHour): static
+    {
+        $this->orderHour = $orderHour;
+
+        return $this;
+    }
+
+    public function getAllergy(): ?string
+    {
+        return $this->allergy;
+    }
+
+    public function setAllergy(?string $allergy): static
+    {
+        $this->allergy = $allergy;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTime $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getClient(): ?User
+    {
+        return $this->client;
+    }
+
+    public function setClient(?User $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getRestaurant(): ?Restaurant
+    {
+        return $this->restaurant;
+    }
+
+    public function setRestaurant(?Restaurant $restaurant): static
+    {
+        $this->restaurant = $restaurant;
+
+        return $this;
+    }
+}
