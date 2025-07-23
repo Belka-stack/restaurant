8211413b6b84cf652447bff3365013d76e272022
Belka-stack <Belkabakouche@gmail.com>
2025-07-23 14:59:19 +0200

    Réalisation de tests unitaires sur les méthodes getApitoken et getRoles de l'entité User.Tests concluants

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -55,6 +55,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(nullable: true)]
     private ?\DateTime $updatedAt = null;
+    
 
     public function getId(): ?int
     {
---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase
+{
+    public function testTheAutomaticApiTokenSettingWhenAnUserIsCreated(): void
+    {
+        $user = new User();
+        $this->assertNull($user->getApiToken());
+    }
+
+    public function testThanAnUserHasAtleastOneRoleUser(): void
+    {
+        $user = new User();
+        $this->assertContains('ROLE_USER', $user->getRoles());
+    }
+}
