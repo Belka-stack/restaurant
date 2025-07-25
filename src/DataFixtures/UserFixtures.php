4d4d2d0b0b0f03e08260065e99ab404a35d10902
Belka-stack <Belkabakouche@gmail.com>
2025-07-25 12:52:04 +0200

    Installation de DoctrineFixturesBundle et création d'une fixture User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use DateTime;
+use Exception;
+use App\Entity\User;
+use Symfony\Component\Uid\Uuid;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher )
+    {
+
+    }
+    /** @throws Exception */
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 20; $i++) {
+
+            $user = (new User())
+                ->setFirstName("Firstname $i")
+                ->setLastName("Lastname $i")
+                ->setGuestNumber(random_int(0,10))
+                ->setEmail("email.$i@studi.fr")
+                ->setCreatedAt(new DateTime())
+                ->setUuid(Uuid::v4()->toRfc4122());
+
+        $user->setPassword($this->passwordHasher->hashPassword($user, "password$i"));
+        $manager->persist($user);
+
+
+        }
+        $manager->flush();
+    }
+}
